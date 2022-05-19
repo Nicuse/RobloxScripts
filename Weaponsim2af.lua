ea9aa89dbda549531bcab1665fc5af1ce6863cf3
Nicuse <92673025+Nicuse@users.noreply.github.com>
2022-05-19 20:36:44 +0500

    Create Weaponsim2af.lua

---
+++ Weaponsim2af.lua
@@ -0,0 +1,6 @@
+-- must hold sword
+
+-- game link:https://www.roblox.com/games/6191637341
+
+game:GetService("Players").LocalPlayer.Character.Humanoid:EquipTool(game.Players.LocalPlayer.Backpack["sword"]) while wait() do game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace")["Current Map"].Baseplate.CFrame game:GetService("Players").LocalPlayer.Character.sword.Handle.pr.RemoteEvent:FireServer() end
+
