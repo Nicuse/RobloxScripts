f2d951d617fae0c786d5ffdcf8dad0308f800f4d
Nicuse <92673025+Nicuse@users.noreply.github.com>
2022-07-12 21:31:51 +0500

    Update MegaEasyObbyComplete.lua

--- MegaEasyObbyComplete.lua
+++ MegaEasyObbyComplete.lua
@@ -1,7 +1,17 @@
 -- // Game: https://www.roblox.com/games/3587619225/Mega-Easy-Obby
 
-local Players = game:GetService("Players")
-local Workspace = game:GetService("Workspace")
-local num = Players.LocalPlayer.leaderstats.Stage.Value + 1
+_G.On = true -- false = turn off, true = true on
 
-Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Workspace.Checkpoints[num].CFrame 
+spawn(function()
+    while _G.On == true do
+        wait(0.2)
+        local Players = game:GetService("Players")
+        local Workspace = game:GetService("Workspace")
+        local Checkpoints = Workspace.Checkpoints
+        local num = Players.LocalPlayer.leaderstats.Stage.Value + 1
+
+        if Checkpoints:FindFirstChild(num) then
+            Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Checkpoints:FindFirstChild(num).CFrame
+        end 
+    end
+end)
