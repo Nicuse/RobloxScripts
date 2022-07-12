4da857364588edd7e8e09ca98c357172bdd4cdaa
Nicuse <92673025+Nicuse@users.noreply.github.com>
2022-07-12 21:48:04 +0500

    Update MegaEasyObbyComplete.lua

--- MegaEasyObbyComplete.lua
+++ MegaEasyObbyComplete.lua
@@ -1,17 +1,44 @@
 -- // Game: https://www.roblox.com/games/3587619225/Mega-Easy-Obby
 
-_G.On = true -- false = turn off, true = true on
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Workspace = game:GetService("Workspace")
+local Checkpoints = Workspace.Checkpoints
+local num = Players.LocalPlayer.leaderstats.Stage.Value + 1
+
+
+_G.On = false -- false = turn off, true = true on
+_G.Prestige = false -- Auto Prestige
 
 spawn(function()
-    while _G.On == true do
-        wait(0.2)
-        local Players = game:GetService("Players")
-        local Workspace = game:GetService("Workspace")
-        local Checkpoints = Workspace.Checkpoints
-        local num = Players.LocalPlayer.leaderstats.Stage.Value + 1
-
-        if Checkpoints:FindFirstChild(num) then
-            Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Checkpoints:FindFirstChild(num).CFrame
-        end 
+    while wait() do
+        if _G.On == true then
+            if Checkpoints:FindFirstChild(num) then
+                Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Checkpoints:FindFirstChild(num).CFrame
+            end
+        end
     end
+end)
+
+spawn(function()
+    while wait(1) do
+        if _G.Prestige == true then
+            ReplicatedStorage.Events.prestige:FireServer()
+        end
+    end
+end)
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local GuiService = game:GetService("GuiService")
+local Window = Library.CreateLib(game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name, "Ocean")
+
+local Tab = Window:NewTab("Main")
+local Section = Tab:NewSection("Main")
+
+Section:NewToggle("Complete Obby", "Completes the obby", function(state)
+    _G.On = state;
+end)
+
+Section:NewToggle("Auto Prestige", "Automatically prestiges when you complete the obby", function(state)
+    _G.Prestige = state;
 end)
